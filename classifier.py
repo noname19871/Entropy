002ff8e5eb023fd98b75c41a02dc1ddf5742afad
noname19871 <smersh197@mail.ru>
2018-04-14 10:57:42 +0300

    created simple linear classifier

--- classifier.py
+++ classifier.py
@@ -1,12 +1,24 @@
+from sklearn.model_selection import cross_val_score
+from sklearn_deltatfidf import DeltaTfidfVectorizer
+from sklearn.svm import LinearSVC
+
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from gensim.summarization.bm25 import get_bm25_weights
-corpus = [
-    ["black", "cat", "white", "cat"],
-    ["cat", "outer", "space"],
-    ["wag", "dog"]
-]
-result = get_bm25_weights(corpus)
-tfidf = TfidfVectorizer(corpus,use_idf=True)
-print(result)
-print(tfidf.idf_)
+
+
+def train(train_csv, str_array):
+    # считываем подготовленный датасет
+    data = pd.read_csv(train_csv, index_col=0).dropna()
+
+    v = DeltaTfidfVectorizer(ngram_range=[1, 2], min_df=2, max_df=0.6)
+
+    X = data['text'].values
+    y = data.mark.astype(int).values.tolist()
+    new_X = v.fit_transform(X, y)
+
+    new_data = v.transform(str_array)
+
+    delta_clf = LinearSVC(penalty='l2')
+
+    delta_clf.fit(new_X, y)
+
+    return delta_clf.predict(new_data)
